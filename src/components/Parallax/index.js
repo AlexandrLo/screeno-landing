a6ff7cab411d0235fc0059b09f7d97c442e9eea9
AlexandrLo <alexandr.lo@outlook.com>
2022-04-06 21:47:41 +0300

    Add responsive values support

--- src/components/Parallax/index.js
+++ src/components/Parallax/index.js
@@ -6,13 +6,20 @@ import {
   useTransform,
   useSpring,
 } from "framer-motion";
-import { Box } from "@chakra-ui/react";
+import { Box, useBreakpointValue } from "@chakra-ui/react";
 
-function Parallax({ children, offset = 50, ...props }) {
+function Parallax({
+  children,
+  offset = { base: 15, md: 25, xl: 50 },
+  ...props
+}) {
   const [elementTop, setElementTop] = useState(0);
   const [clientHeight, setClientHeight] = useState(0);
   const ref = useRef(null);
   const { scrollY } = useViewportScroll();
+  const offsetBreakpoints =
+    typeof offset === "object" ? offset : { base: offset };
+  const responsiveOffset = useBreakpointValue(offsetBreakpoints);
 
   const onResize = () => {
     setElementTop(
@@ -29,8 +36,12 @@ function Parallax({ children, offset = 50, ...props }) {
   }, [ref]);
 
   const initial = elementTop - clientHeight;
-  const final = elementTop + offset;
-  const yTransform = useTransform(scrollY, [initial, final], [offset, -offset]);
+  const final = elementTop + responsiveOffset;
+  const yTransform = useTransform(
+    scrollY,
+    [initial, final],
+    [responsiveOffset, -responsiveOffset],
+  );
   const y = useSpring(yTransform, { stiffness: 400, damping: 90 });
 
   return (
