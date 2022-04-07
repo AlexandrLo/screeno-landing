c6f5e970d5f6de1b1c41303e304ef374f46bccbc
AlexandrLo <alexandr.lo@outlook.com>
2022-04-07 20:24:08 +0300

    Add Heading size

--- src/theme/components/Heading.js
+++ src/theme/components/Heading.js
@@ -1,11 +1,16 @@
 const Heading = {
   sizes: {
-    sm: {
+    h3: {
       fontSize: "18px",
       fontWeight: "700",
       lineHeight: "1.2",
     },
-    md: {
+    h2: {
+      fontSize: ["24px", "30px", "36px"],
+      fontWeight: "700",
+      lineHeight: "1.2",
+    },
+    h1: {
       fontSize: ["36px", "40px", "48px"],
       fontWeight: "800",
       lineHeight: "1.2",
